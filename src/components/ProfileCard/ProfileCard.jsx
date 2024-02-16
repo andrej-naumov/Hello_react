770aace3bd7f9e27ff887054e6b97dd3fa21436a
andrej-naumov <andrejs@list.ru>
2024-02-16 13:17:43 +0100

    added Lesson 17 with ProfileCard

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,13 @@
+function ProfileCard() {
+    return (
+        <div className="profile-card">
+            <img src="media\img\avatar – Google Поиск.png" alt="Аватар" />
+            <h2>Имя Фамилия</h2>
+            <p>Род деятельности</p>
+            <p>Цвет волос: цвет_волос, Рост: рост</p>
+            <p>Хобби: Хобби</p>
+        </div>
+    );
+}
+
+export default ProfileCard;
