ae2ad2882cb7ac733e6daeba2a0e038cec70495d
andrej-naumov <andrejs@list.ru>
2024-02-16 20:36:43 +0100

    added const for ProfileCard

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,11 +1,28 @@
+import './styles.css';
+
+const profile = {
+    src: "media/img/avatar – Google Поиск.png",
+    firstName: "Имя",
+    lastName: "Фамилия",
+    job: "art of job",
+    hairColor: "hair color",
+    height: "180cm",
+    hobby: "art of hobby",
+    age: 31 // if > 50 - another color at name
+}
+
+const normalizedName = () => { return `${profile.firstName} ${profile.lastName}` };
+
+
 function ProfileCard() {
     return (
         <div className="profile-card">
-            <img src="media\img\avatar – Google Поиск.png" alt="Аватар" />
-            <h2>Имя Фамилия</h2>
-            <p>Род деятельности</p>
-            <p>Цвет волос: цвет_волос, Рост: рост</p>
-            <p>Хобби: Хобби</p>
+            <img src={profile.src} alt="Аватар" />
+            <h2>{profile.firstName} {profile.lastName}</h2>
+            <h2 className={(profile.age) > 50 ? "senior" : "junior"}>{normalizedName()}</h2>
+            <p>Работа - {profile.job}</p>
+            <p>Цвет волос: {profile.hairColor}, Рост: {profile.height}</p>
+            <p>Хобби: {profile.hobby}</p>
         </div>
     );
 }
