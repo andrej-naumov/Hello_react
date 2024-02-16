ae838a15840bf12e0c5a863b0af1d5b85877a1a3
andrej-naumov <andrejs@list.ru>
2024-02-16 20:56:56 +0100

    added data,js with profile const

---
+++ src/components/ProfileCard/data.js
@@ -0,0 +1,10 @@
+export const profile = {
+    src: "media/img/avatar – Google Поиск.png",
+    firstName: "Имя",
+    lastName: "Фамилия",
+    job: "art of job",
+    hairColor: "hair color",
+    height: "180cm",
+    hobby: "art of hobby",
+    age: 31 // if > 50 - another color at name
+}
