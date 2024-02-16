bb86405d2e9fdffcbbec28af8f56960ee80b965a
andrej-naumov <andrejs@list.ru>
2024-02-16 13:21:41 +0100

    added Lesson 17 --- Profile Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import './App.css';
 import Button from './components/Button/Button';
+import ProfileCard from './components/ProfileCard/ProfileCard';
 
 function App() {
 
-  const nameOfCard = "Zaporozhec";
+  // const nameOfCard = "Zaporozhec";
 
-  const cars = { car1: "Volvo", car2: "Audi", car3: "Lada" };
+  // const cars = { car1: "Volvo", car2: "Audi", car3: "Lada" };
 
   return (
     <div className="App">
+      {/*       
       <p>React works - app lesson 17</p>
       <div className='button-container'>
         <Button /><Button /><Button />
       </div>
       <div className='car-card'>{cars.car1}
       <img src="https://www.topgear.com/sites/default/files/2023/03/1%20Volvo%20XC90.jpg" alt="" />
-      </div>
+      </div>*/}
+
+      <ProfileCard /><ProfileCard /><ProfileCard />
+
     </div>
+
   );
 }
 
