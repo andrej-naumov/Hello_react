576ee086135021953bfef7ed418dd5a1fe3403d0
andrej-naumov <andrejs@list.ru>
2024-02-16 21:00:12 +0100

    added import profile  from data

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,15 +1,6 @@
 import './styles.css';
 
-const profile = {
-    src: "media/img/avatar – Google Поиск.png",
-    firstName: "Имя",
-    lastName: "Фамилия",
-    job: "art of job",
-    hairColor: "hair color",
-    height: "180cm",
-    hobby: "art of hobby",
-    age: 31 // if > 50 - another color at name
-}
+import {profile} from './data';
 
 const normalizedName = () => { return `${profile.firstName} ${profile.lastName}` };
 
@@ -18,7 +9,7 @@ function ProfileCard() {
     return (
         <div className="profile-card">
             <img src={profile.src} alt="Аватар" />
-            <h2>{profile.firstName} {profile.lastName}</h2>
+            {/* <h2>{profile.firstName} {profile.lastName}</h2> */}
             <h2 className={(profile.age) > 50 ? "senior" : "junior"}>{normalizedName()}</h2>
             <p>Работа - {profile.job}</p>
             <p>Цвет волос: {profile.hairColor}, Рост: {profile.height}</p>
